1c258a2a3c9c5c15b9c5cc15679555b3a884d0d7
Ivy7493 <ivanblizz23@gmail.com>
2020-03-12 10:11:46 +0200

    added some basic database creation commands, these can be edited when the final layout of the tables has been established

--- DatabasesAPI.py
+++ DatabasesAPI.py
@@ -10,3 +10,36 @@ def ConnectDatabase():
     c = conn.cursor()
     print("Successfully connected to Database!")
 
+#command to add fridge to database 
+def AddFridge(_conn, _FridgeID, _Mboxes, _Temparture):
+    Temp = _conn.cursor()
+    FridgeID = _FridgeID
+    Cboxes = 0
+    Mboxes = _Mboxes
+    Temparture = _Temparture
+    command = "INSERT INTO Fridges VALUES (" + "'" + FridgeID + "'" + ", " + str(Cboxes) + ", " +   str(Mboxes) + ", " + str(Temparture) + ")" 
+    Temp.execute(command)
+    _conn.commit()
+
+#command for returning the number of fridges in database
+def ReturnNumberOfFridges(_conn):
+    Temp = _conn.cursor()
+    Results = Temp.execute("SELECT * FROM Fridges")
+    count = 0
+    for Result in Results:
+        count++
+    return count
+
+#basic function for creating the fridge table in the database
+def CreateFridgeTable(_conn):
+    Temp = _conn.cursor()
+    Temp.execute("""CREATE TABLE Fridges(
+                   FridgeID TEXT NOT NULL PRIMARY KEY,
+                   Cboxes INTEGER,
+                   Mboxes INTEGER,
+                   Tempature INTEGER
+                   )""")
+    _conn.commit()
+
+def GetFile(Filename);
+
