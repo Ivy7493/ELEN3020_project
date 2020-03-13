9c039b9aca9c9b9404aa5939c163346f48552b97
Ivy7493 <ivanblizz23@gmail.com>
2020-03-13 12:05:15 +0200

    Added Box control, - Addbox funtion, -IsFridgefull function, -Fridgeboxcount function, -PrintBoxTable function

--- DatabasesAPI.py
+++ DatabasesAPI.py
@@ -15,15 +15,14 @@ def ConnectDatabase():
 def AddFridge(_conn, _FridgeID, _Mboxes, _Temparture):
     Temp = _conn.cursor()
     FridgeID = _FridgeID
-    Cboxes = 0
     Mboxes = _Mboxes
     Temparture = _Temparture
-    command = "INSERT INTO Fridges VALUES (" + "'" + FridgeID + "'" + ", " + str(Cboxes) + ", " +   str(Mboxes) + ", " + str(Temparture) + ")" 
+    command = "INSERT INTO Fridges VALUES (" + "'" + FridgeID + "'"  + ", " +   str(Mboxes) + ", " + str(Temparture) + ")" 
     Temp.execute(command)
     _conn.commit()
 
 
-#Not confirmed working, checking to see if FridgeID given is unique
+#confirmed working, checking to see if FridgeID given is unique
 def CheckFridgeID(_conn, _FridgeID):
     Temp = _conn.cursor()
     Temp.execute("SELECT * FROM Fridges WHERE FridgeID=?", (_FridgeID,))
@@ -49,10 +48,78 @@ def CreateFridgeTable(_conn):
     Temp = _conn.cursor()
     Temp.execute("""CREATE TABLE Fridges(
                    FridgeID TEXT NOT NULL PRIMARY KEY,
-                   Cboxes INTEGER,
                    Mboxes INTEGER,
                    Tempature INTEGER
                    )""")
     _conn.commit()
+
+#prints all the fridge entrys to console
+def PrintFridgeTable(_conn):
+    Temp = _conn.cursor()
+    x = Temp.execute("SELECT * FROM Fridges")
+    for y in x:
+        print(y)
+
+def CreateBoxTable(_conn):
+    Temp = _conn.cursor()
+    Temp.execute("""CREATE TABLE Boxes(
+                        BoxID TEXT NOT NULL PRIMARY KEY,
+                        FridgeID TEXT NOT NULL,
+                        Msamples INTEGER,
+                        FOREIGN KEY (FridgeID) REFERENCES Fridges (FridgeID)
+                        )""")
+    _conn.commit()
+
+def PrintBoxTable(_conn):
+    Temp = _conn.cursor()
+    x = Temp.execute("SELECT * FROM Boxes")
+    for y in x:
+        print(y)
+
+#Function to add box to database
+def AddBox(_conn,_BoxID, _FridgeID, _Msamples):
+    Temp = _conn.cursor()
+    if CheckFridgeID(_conn, _FridgeID) == "False" and IsFridgeFull(_conn, _FridgeID) == "False":
+        command = "INSERT INTO Boxes VALUES (" + "'" + _BoxID + "'"  + ", " + "'" + _FridgeID + "'" + ", " +   str(_Msamples) + ")" 
+        Temp.execute(command)
+        _conn.commit()
+    elif CheckFridgeID(_conn, _FridgeID) == "True":
+        print("Not a valid entry, FridgeID does not exist")
+    elif IsFridgeFull(_conn, _FridgeID) == "True":
+        print("Not a Valid entry, Fridge is full and cannot take another box")
+
+#check to see if fridge can hold another box
+def IsFridgeFull(_conn, _FridgeID):
+    Max = ReturnFridgeSize(_conn, _FridgeID)
+    count = ReturnFridgeBoxCount(_conn, _FridgeID)
+    print("count = " + str(count))
+    print("Max = " + str(Max))
+    if count < Max:
+        print("Fridge Has space")
+        return "False"
+    elif count >= Max:
+        print("Fridge Has No space")
+        return "True"
+
+#Returns the max capicty of fridge
+def ReturnFridgeSize(_conn, _FridgeID):
+    Temp = _conn.cursor()
+    Temp.execute("SELECT Mboxes FROM Fridges WHERE FridgeID = " + "'" + _FridgeID + "'")
+    Max = Temp.fetchall()
+    Max = Max[0][0]
+    return Max
+
+#Returns the number of Boxes in a fridge
+def ReturnFridgeBoxCount(_conn, _FridgeID):
+    Temp = _conn.cursor()
+    Temp.execute("SELECT * FROM Boxes WHERE FridgeID = " + "'" + _FridgeID + "'")
+    Current = Temp.fetchall()
+    print(Current)    
+    count = 0    
+    for x in Current:
+        count = count + 1
+    return count     
+    
+    
 
 
--- TestUI.py
+++ TestUI.py
@@ -2,15 +2,43 @@ import DatabasesAPI
 import sqlite3
 from tkinter import *
 
+def GetFridgeInfo():
+    _FridgeID = FridgeIDUI.get()
+    _MaxBoxes = MaxBoxesUI.get()
+    _Temperature = TemperatureUI.get()
+    DatabasesAPI.AddFridge(conn,_FridgeID,_MaxBoxes,_Temperature)
+
 #This is for testing the back end. DO NOT EDIT OR REMOVE
 conn = DatabasesAPI.ConnectDatabase()
-print(DatabasesAPI.CheckFridgeID(conn, "ABC"))
+c = conn.cursor()
+c.execute("PRAGMA foreign_keys = ON")
+#DatabasesAPI.CreateFridgeTable(conn)
+#DatabasesAPI.CreateBoxTable(conn)
+DatabasesAPI.AddBox(conn, "DXD", "ADC", 10)
+DatabasesAPI.IsFridgeFull(conn, 'ABC')
+DatabasesAPI.PrintBoxTable(conn)
 print(DatabasesAPI.ReturnNumberOfFridges(conn))
+DatabasesAPI.PrintFridgeTable(conn)
 win = Tk()
 MainTitle = Label(win,text="Back end testing UI!")
 MainTitle.grid(row=0,column=1)
 Connect = Button(win,text="Connect to Database",command=DatabasesAPI.ConnectDatabase)
 Connect.grid(row=1,column=1)
+FridgeIDUI = Entry(win)
+MaxBoxesUI = Entry(win)
+TemperatureUI = Entry(win)
+FridgeIDUI.grid(row=3,column=0)
+MaxBoxesUI.grid(row=3,column=1)
+TemperatureUI.grid(row=3,column=2)
+FridgeIDLabel = Label(win,text="Enter Fridge ID")
+MaxBoxesLabel = Label(win,text="Enter max boxes")
+TemperatureLabel = Label(win,text="Enter Fridge Temp")
+FridgeIDLabel.grid(row=2,column=0)
+MaxBoxesLabel.grid(row=2,column=1)
+TemperatureLabel.grid(row=2,column=2)
+SubmitFridge = Button(win,text="Add Fridge",command=GetFridgeInfo)
+SubmitFridge.grid(row=4,column=1)
+
 
 
 win.mainloop()
