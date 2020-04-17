a7ed2cddd6b5e80c2c3d2939b843e5ddcec608a4
JessWhosBack <jessevdmerwe@gmail.com>
2020-04-17 10:45:18 +0200

    added back buttons to search screen, changed to passing conn instead of initializing conn

--- DisplayFridges.py
+++ DisplayFridges.py
@@ -2,110 +2,117 @@ import sqlite3
 import tkinter as tk
 from tkinter import ttk
 
-def OpenAllFridges():
-    conn = sqlite3.connect('Test.db')
-    conn.execute("PRAGMA foreign_keys = ON")
+def OpenAllFridges(conn):
     c = conn.cursor()
 
     window_Fridges = tk.Tk()
     window_Fridges.title("FRIDGES")
 
-    cols = ('Fridge ID', 'Temperature', 'NumShelves', 'NumBoxes', 'BoxWidth', 'BoxLength', 'BoxHeight')
+    cols = ('Fridge ID', 'Temperature', 'NumShelves', 'WidthShelves')
     tree = ttk.Treeview(window_Fridges, columns=cols, show='headings')
     for col in cols:
         tree.heading(col, text=col)
     tree.grid(row=2, column=0, columnspan=7)
 
-    c.execute("SELECT * FROM fridgeTable ")
+    c.execute("SELECT * FROM FridgeTable")
 
     for row in c.fetchall():
         tree.insert("", "end", values = (row))
         print(row)
 
+    def openFridgeSearchMenu():
+        window_Fridges.destroy()
+        MainFridge_Window()
 
+    backButton = tk.Button(window_Fridges, text = 'Back to Search Menu', command = openFridgeSearchMenu).grid(row=5, column=1)
+    
     window_Fridges.mainloop()
-    c.close()
-    conn.close()
 #----------------------------------------------------------------------------------------
     
 
 #----------------------------------------------------------------------------------------
-def OpenFridgeSearch(searchField):
-    conn = sqlite3.connect('Test.db')
+def OpenFridgeSearch(conn, searchField):
     c = conn.cursor()
 
     window_Fridges = tk.Tk()
     window_Fridges.title("FRIDGES")
 
-    cols = ('Fridge ID', 'Temperature', 'NumShelves', 'NumBoxes', 'BoxWidth', 'BoxLength', 'BoxHeight')
+    cols = ('Fridge ID', 'Temperature', 'NumShelves', 'WidthShelves')
     tree = ttk.Treeview(window_Fridges, columns=cols, show='headings')
     for col in cols:
         tree.heading(col, text=col)
     tree.grid(row=2, column=0, columnspan=7)
 
-    c.execute("SELECT * FROM fridgeTable WHERE fridgeID=?", (str(searchField),))
+    c.execute("SELECT * FROM FridgeTable WHERE fridgeID=?", (str(searchField),))
 
     for row in c.fetchall():
         tree.insert("", "end", values = (row))
         print(row)
 
+    def openFridgeSearchMenu():
+        window_Fridges.destroy()
+        MainFridge_Window()
 
+    backButton = tk.Button(window_Fridges, text = 'Back to Search Menu', command = openFridgeSearchMenu).grid(row=5, column=1)
+    
     window_Fridges.mainloop()
-    c.close()
-    conn.close()
 #----------------------------------------------------------------------------------------
 
 
 #----------------------------------------------------------------------------------------
-def OpenTemperatureSearch(searchField):
-    conn = sqlite3.connect('Test.db')
+def OpenTemperatureSearch(conn, searchField):
     c = conn.cursor()
 
     window_Fridges = tk.Tk()
     window_Fridges.title("FRIDGES")
 
-    cols = ('Fridge ID', 'Temperature', 'NumShelves', 'NumBoxes', 'BoxWidth', 'BoxLength', 'BoxHeight')
+    cols = ('Fridge ID', 'Temperature', 'NumShelves', 'WidthShelves')
     tree = ttk.Treeview(window_Fridges, columns=cols, show='headings')
     for col in cols:
         tree.heading(col, text=col)
     tree.grid(row=2, column=0, columnspan=7)
 
-    c.execute("SELECT * FROM fridgeTable WHERE temperature=?", (int(searchField),))
+    c.execute("SELECT * FROM FridgeTable WHERE temperature=?", (int(searchField),))
 
     for row in c.fetchall():
         tree.insert("", "end", values = (row))
         print(row)
 
+    def openFridgeSearchMenu():
+        window_Fridges.destroy()
+        MainFridge_Window()
+
+    backButton = tk.Button(window_Fridges, text = 'Back to Search Menu', command = openFridgeSearchMenu).grid(column=1)
 
     window_Fridges.mainloop()
-    c.close()
-    conn.close()
 #----------------------------------------------------------------------------------------
 
 
 #----------------------------------------------------------------------------------------
-def OpenNumShelvesSearch(searchField):
-    conn = sqlite3.connect('test.db')
+def OpenNumShelvesSearch(conn, searchField):
     c = conn.cursor()
 
     window_Fridges = tk.Tk()
     window_Fridges.title("FRIDGES")
 
-    cols = ('Fridge ID', 'Temperature', 'NumShelves', 'NumBoxes', 'BoxWidth', 'BoxLength', 'BoxHeight')
+    cols = ('Fridge ID', 'Temperature', 'NumShelves', 'WidthShelves')
     tree = ttk.Treeview(window_Fridges, columns=cols, show='headings')
     for col in cols:
         tree.heading(col, text=col)
     tree.grid(row=2, column=0, columnspan=7)
 
-    c.execute("SELECT * FROM fridgeTable WHERE numShelves=?", (int(searchField),))
+    c.execute("SELECT * FROM FridgeTable WHERE numShelves=?", (int(searchField),))
 
     for row in c.fetchall():
         tree.insert("", "end", values = (row))
         print(row)
 
+    def openFridgeSearchMenu():
+        window_Fridges.destroy()
+        MainFridge_Window()
+
+    backButton = tk.Button(window_Fridges, text = 'Back to Search Menu', command = openFridgeSearchMenu).grid(row=5, column=1)
 
     window_Fridges.mainloop()
-    c.close()
-    conn.close()
 #----------------------------------------------------------------------------------------
 
